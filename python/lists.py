339cb5b18c0bce1f10b78f674aa1bc0a72c0ccf3
jahon <danaboev.jf@gmail.com>
2023-10-31 08:19:43 +0500

    pyhin_hello

---
+++ python/lists.py
@@ -0,0 +1,9 @@
+# Define a list a names
+names = ["Harry", "Ron", "Ginny", "John"]
+print(names[0])
+
+names.append("Draco")
+
+names.sort()
+
+print(names)
---
+++ python/sequences.py
@@ -0,0 +1,13 @@
+name = "John"
+print(name[0])
+#---------------------------------------------------------------
+names = ["John", "Harry", "Tom"]
+print(names[2])
+#---------------------------------------------------------------
+#list tuple set dict ...
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
+
+
